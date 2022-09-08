edfd090e8de49a840a5e668c81750f968de8f440
montiho8 <montiho8@hawaii.edu>
2022-09-07 20:38:28 -1000

    Completed Swift Playgrounds One Part 8
    Added three new variables and assigned to different strings
    
    Added fourth variable to combine three new variables
    
    added print function for fourth variable

--- SwiftPlaygroundOne.playground/Contents.swift
+++ SwiftPlaygroundOne.playground/Contents.swift
@@ -12,3 +12,17 @@ var mySecondVariable = "and look I added two strings together"
 
 var myThirdVariable = myFirstVariable + mySecondVariable
 print(myThirdVariable)
+
+var myFourthVariable: String
+myFourthVariable = "I created my new string"
+
+var myFifthVariable: String
+myFifthVariable = " Hello My Name "
+
+//We're going to do something with strings
+var mySixthVariable = " and look I added three strings together "
+
+
+var mySeventhVariable = myFourthVariable + myFifthVariable + mySixthVariable
+print(mySeventhVariable)
+
